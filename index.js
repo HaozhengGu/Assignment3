176a1c8a6902479c420312d96cced87da772d446
Haozheng Gu <sa19b014@technikum-wien.at>
2021-10-25 00:21:29 +0200

    Assignment 2

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btnAddRevenue = window.document.getElementById("add-revenue");
-    btnAddRevenue.addEventListener("click", handleClick1);
+    btnAddRevenue.addEventListener("click", handleClickRev);
     const btnAddExpense = window.document.getElementById("add-expense");
-    btnAddExpense.addEventListener("click", handleClick2);
-
-    //const input = document.getElementById("input");
-    //input.addEventListener("keydown", handleKeyDown);
+    btnAddExpense.addEventListener("click", handleClickExp);
 });
 
 function setTwoNumberDecimal(event) {
@@ -13,104 +10,122 @@ function setTwoNumberDecimal(event) {
     event.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
 }
 
-function handleClick1(event) {
+function handleClickRev(event) {
     addRevenue();
 }
 
-function handleClick2(event) {
+function handleClickExp(event) {
     addExpense();
 }
 
-/*
-function handleKeyDown(event) {
-    if (event.key === "Enter") {
-        add();
-    }
-}
-*/
-
 function addRevenue() {
     const revList = document.getElementById("revenue-list");
-    const revInput = document.getElementById("input");
+    const revInput = document.getElementById("revenue-input");
     const revText = revInput.value;
-    
+
+    while (revText == null || revText == "") {
+        alert("Please enter the amount.");
+        return false;
+    }
+
+    const rev = document.getElementsByName("rev");
+
     const revItem = document.createElement("li");
-    revItem.textContent = '+' + revText + ' €';
+
+    if (document.getElementById("wages").checked == true) {
+        revItem.textContent = '+' + revText + ' € ' + rev[0].value;
+    } else if (document.getElementById("trades").checked == true) {
+        revItem.textContent = '+' + revText + ' € ' + rev[1].value;
+    } else if (document.getElementById("stocks").checked == true) {
+        revItem.textContent = '+' + revText + ' € ' + rev[2].value;
+    } else if (document.getElementById("others-rev").checked == true) {
+        revItem.textContent = '+' + revText + ' € ' + rev[3].value;
+    } else {
+        revItem.textContent = '+' + revText + ' € ';
+    }
+
     revItem.addEventListener("click", removeRevenue);
     revInput.value = "";
     revInput.focus();
     
     revList.appendChild(revItem);
     
-    const list = [];
+    const arrMaxRev = [];
     
     for (let i = 0; i < revList.children.length; i++) {
-        const num = parseFloat(revList.children[i].textContent); 
-        list.push(num);
+        const numRev = parseFloat(revList.children[i].textContent); 
+        arrMaxRev.push(numRev);
     }
-    const max = Math.max(...list);
+    const maxNumRev = Math.max(...arrMaxRev);
     
     const maxRev = document.getElementById("max-revenue");
-    const oRevItem = document.createElement("li");
-    oRevItem.textContent = '+' + max + ' €';
+    maxRev.textContent = '+' + maxNumRev + ' €';
 
-    maxRev.appendChild(oRevItem);
+    const arrSumRev = [];
 
-    const s = [];
-
-    for (let i = 0; i < revList.children.length; i++) {
-        const arr = parseFloat(revList.children[i].textContent);
-        s.push(arr);
+    for (let j = 0; j < revList.children.length; j++) {
+        const arrRev = parseFloat(revList.children[j].textContent);
+        arrSumRev.push(arrRev);
     }
-    const sum = eval(s.join("+"));
+    const sumArrRev = eval(arrSumRev.join("+"));
 
     const sumRev = document.getElementById("sum-revenue");
-    const sRevItem = document.createElement("li");
-    sRevItem.textContent = '+' + sum + ' €';
-
-    sumRev.appendChild(sRevItem);
+    sumRev.textContent = '+' + sumArrRev + ' €';
 }
 
 function addExpense() {
     const expList = document.getElementById("expense-list");
-    const expInput = document.getElementById("input");
+    const expInput = document.getElementById("expense-input");
     const expText = expInput.value;
+
+    while (expText == null || expText == "") {
+        alert("Please enter the amount.");
+        return false;
+    }
     
+    const exp = document.getElementsByName("exp");
+
     const expItem = document.createElement("li");
-    expItem.textContent = '-' + expText + ' €';
+
+    if (document.getElementById("repasts").checked == true) {
+        expItem.textContent = '-' + expText + ' € ' + exp[0].value;
+    } else if (document.getElementById("shopping").checked == true) {
+        expItem.textContent = '-' + expText + ' € ' + exp[1].value;
+    } else if (document.getElementById("amusements").checked == true) {
+        expItem.textContent = '-' + expText + ' € ' + exp[2].value;
+    } else if (document.getElementById("others-exp").checked == true) {
+        expItem.textContent = '-' + expText + ' € ' + exp[3].value;
+    } else {
+        expItem.textContent = '-' + expText + ' € ';
+    }
+
     expItem.addEventListener("click", removeExpense);
     expInput.value = "";
     expInput.focus();
     
     expList.appendChild(expItem);
     
-    const list = [];
+    const arrMaxExp = [];
     
     for (let i = 0; i < expList.children.length; i++) {
-        const num = parseFloat(expList.children[i].textContent);
-        list.push(num);
+        const numExp = parseFloat(expList.children[i].textContent);
+        arrMaxExp.push(numExp);
     }
-    const max = Math.min(...list);
+    const maxNumExp = Math.min(...arrMaxExp);
     
     const maxExp = document.getElementById("max-expense");
-    const oExpItem = document.createElement("li");
-    oExpItem.textContent = max + ' €';
+    maxExp.textContent = maxNumExp + ' €';
 
-    maxExp.appendChild(oExpItem);
+    const arrSumExp = [];
 
-    const s = [];
-
-    for (let i = 0; i < expList.children.length; i++) {
-        const arr = parseFloat(expList.children[i].textContent);
-        s.push(arr);
+    for (let j = 0; j < expList.children.length; j++) {
+        const arrExp = parseFloat(expList.children[j].textContent);
+        arrSumExp.push(arrExp);
     }
-    const sum = eval(s.join("+"));
+    const sumArrExp = eval(arrSumExp.join("+"));
     
     const sumExp = document.getElementById("sum-expense");
-    const sExpItem = document.createElement("li");
-    sExpItem.textContent = sum + ' €';
-
-    sumExp.appendChild(sExpItem);
+    sumExp.textContent = sumArrExp + ' €';
 }
 
 function removeRevenue(event) {
